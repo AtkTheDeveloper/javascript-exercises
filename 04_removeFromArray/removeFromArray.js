bfb613da03ae4350a478d90850e884eca77c58a5
Atk The Developer <182418129+AtkTheDeveloper@users.noreply.github.com>
2024-12-10 17:09:29 -0300

    Passed only the first test. Wasn't able to do the rest so I had to result to the solution for the answer. I am quite disappointed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    return array.filter(item => !num.includes(item));
 };
 
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
