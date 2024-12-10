ff05f52c8d96c8076ed63cd0c9d5d4a669f8c631
Atk The Developer <182418129+AtkTheDeveloper@users.noreply.github.com>
2024-12-10 15:42:00 -0300

    Finished the repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = '';
 
+    for(let i = 0; i < num; i++){
+        repeatedString +=  string;
+    }
+
+    if(num < 0){
+        return 'ERROR';
+    } else{
+        return repeatedString;
+    }
 };
+
+const number = Math.floor(Math.random() * 1000);
+
+repeatString('', number);
 
 // Do not edit below this line
 module.exports = repeatString;
