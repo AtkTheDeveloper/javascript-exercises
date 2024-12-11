16c2c7a51f0b36f17420c95d3f0eae17bdfd3d87
Atk The Developer <182418129+AtkTheDeveloper@users.noreply.github.com>
2024-12-11 09:30:22 -0300

    Passed all the test in the sumAll exercise. It was very simple. Just had to do some googling and asking chatgpt for explanations not code.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+
+  let sum = 0;
+
+  if(num1 < 0 || num2 < 0){
+    return "ERROR";
+  }
+
+  if(!Number.isInteger(num1) || !Number.isInteger(num2) ){
+    return "ERROR";
+  }
+
+  if(isNaN(num1) || isNaN(num2) ){
+    return "ERROR";
+  }
+
+  if (Array.isArray(num1) || Array.isArray(num2)) {
+    return "ERROR";
+  }
+
+  if (num1 <= num2) {
+
+    for (let i = num1; i <= num2; i++) {
+      sum += i;
+    }
+
+  } else {
+
+    for (let i = num1; i >= num2; i--) {
+      sum += i;
+    }
+
+  }
+
+  return sum;
 
 };
+
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
